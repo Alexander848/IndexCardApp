d79b4fdddc5aa30d6828124b8f610fce850e4384
Alexander Gliese <alexander.gliese@web.de>
2025-08-10 09:47:50 +0200

    Added DataAccess+Model, Renamed projects, implemented Model

---
+++ Model/IndexCard.cs
@@ -0,0 +1,12 @@
+﻿namespace IndexCardAppModel
+{
+    public class IndexCard
+    {
+        public long Id { get; set; }
+        public long SetId { get; set; }
+        public string Name { get; set; } = "No Name";
+        public string Question { get; set; } = "No Question";
+        public string Answer { get; set; } = "No Answer";
+        public int AlreadyAnsweredCount { get; set; }
+    }
+}
---
+++ Model/IndexCardSet.cs
@@ -0,0 +1,11 @@
+﻿using IndexCardAppModel;
+
+namespace Model
+{
+    internal class IndexCardSet
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = "No Name";
+        public List<IndexCard> IndexCards { get; set; } = new();
+    }
+}
